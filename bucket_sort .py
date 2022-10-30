28aabd527a73b94a39c2eb20855b8f89fe69b6e7
simple kumari <simple732003@gmail.com>
2022-10-30 10:31:03 +0530

    modified bucket sort

--- bucket_sort .py
+++ bucket_sort .py
@@ -1,11 +1,11 @@
 def insertion_Sort(arr):
     for i in range(1, len(arr)):
-        ans = arr[i]
+        answer = arr[i]
         j = i - 1
-        while j >= 0 and arr[j] > ans: 
+        while j >= 0 and arr[j] > answer: 
             arr[j + 1] = arr[j]
             j -= 1
-        arr[j + 1] = ans    
+        arr[j + 1] = answer    
     return arr     
 			
 def Bucket__Sort(brr):
