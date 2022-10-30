8e39ea9be331657f59f4ae04cba8b09c7828e104
simple kumari <103820895+SimpleKumari@users.noreply.github.com>
2022-10-30 10:04:30 +0530

    Create bucket_sort .py
    bucket sort DSA inplementation code

---
+++ bucket_sort .py
@@ -0,0 +1,32 @@
+def insertion_Sort(arr):
+    for i in range(1, len(arr)):
+        ans = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > ans: 
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = ans    
+    return arr     
+			
+def Bucket__Sort(brr):
+	P = []
+	s = 10 
+	for i in range(s):
+		P.append([])
+	for j in brr:
+		G = int(s * j)
+		P[G].append(j)
+	for i in range(s):
+		P[i] = insertion_Sort(P[i])
+	count= 0
+	for i in range(s):
+		for j in range(len(P[i])):
+			brr[count] = P[i][j]
+			count += 1
+	return brr
+
+
+x = [0.83234, 0.565, 0.666666,0.897978, 0.444, 0.3439]
+print("Sorted Array : ")
+print(Bucket__Sort(x))
+
